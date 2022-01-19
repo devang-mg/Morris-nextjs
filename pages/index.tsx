6d1f9f7204ba432639d16867a7525173f0b715b0
Liam Davis <liamdavis@tuta.io>
2022-01-19 13:26:46 -0500

    init

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<section className="bg-[#141F2B] h-screen flex items-center justify-center">
+			<h1 className="text-white text-xl font-semibold text-center">Monogram</h1>
+		</section>
+	)
+}
+
+export default Home
