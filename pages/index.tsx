503c9b16a4db6a8dfb1f84c69491f1eb3f76b145
Liam Davis <liamdavis@tuta.io>
2022-01-19 14:10:43 -0500

    small style update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 const Home = () => {
 	return (
-		<section className="bg-[#141F2B] h-screen flex items-center justify-center">
-			<h1 className="text-white text-xl font-semibold text-center">Monogram</h1>
+		<section className="container bg-[#141F2B] h-screen py-20 space-y-2 flex flex-col items-center text-white">
+			<h1 className="text-xl font-semibold">Monogram</h1>
+			<p className="text-lg">Next UI</p>
 		</section>
 	)
 }
